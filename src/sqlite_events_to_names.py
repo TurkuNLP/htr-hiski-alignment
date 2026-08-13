b152318352c611bc344bec0387395e2a03b735ba
Eetu Hongisto <eetujhongisto@gmail.com>
2026-08-13 17:05:05 +0300

    function for db query from moving direction

--- src/sqlite_events_to_names.py
+++ src/sqlite_events_to_names.py
@@ -24,3 +24,29 @@ def parish_events_to_df(cursor: sqlite3.Cursor, table: str, parish_id: int):
     events_df = events_df.fillna("")
 
     return events_df
+
+def db_query_and_params_from_direction(moving_direction, event_ids):
+    placeholders = ", ".join(["?"] * len(event_ids))
+    query_params = list(event_ids)
+
+    query = ""
+    if moving_direction in ["", "both", "unknown"]:
+        query_params += list(event_ids)
+
+        query = f"""
+            SELECT * FROM immigrated WHERE event_id IN ({placeholders})
+            UNION ALL
+            SELECT * FROM emigrated WHERE event_id IN ({placeholders})
+        """
+    elif moving_direction.startswith("in"):
+        query = f"""
+            SELECT * FROM immigrated
+            WHERE event_id IN ({placeholders});
+        """
+    elif moving_direction.startswith("out"):
+        query = f"""
+            SELECT * FROM emigrated
+            WHERE event_id IN ({placeholders});
+        """
+
+    return query, query_params
